4e5e8d9cf3b1f7c7ebce45666b0d340b5a1921ee
sshodge2 <sshodge2@ncsu.edu>
2022-04-25 17:09:20 -0400

    wait for reset time

--- find_repos.py
+++ find_repos.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
-from github import Github
-from decouple import config
+from github import Github       #pip3 install pyGithub
+from decouple import config     #pip3 install python-decouple
 from time import sleep
+import datetime
+
+from os import path
+
+search_term_list = ['behaviortree_ros']
+
+if not path.exists('.env'):
+    raise Exception('Please create .env file')
 
 github_oaut_token = config('GITHUB_TOKEN')
 
@@ -12,3 +20,19 @@ g = Github(login_or_token=github_oaut_token)
 remaining, request_limit = g.rate_limiting
 
 print("Remaining: %s, Limit: %s" % (remaining, request_limit))
+
+while(True):
+
+    while(remaining > 1):
+        sleep(10)
+        remaining, request_limit = g.rate_limiting
+        # print("Remaining: %s, Limit: %s" % (remaining, request_limit))
+
+    
+
+    reset_time = g.rate_limiting_resettime
+
+    # sleep until the reset time
+    sleep(reset_time - datetime.datetime.now().timestamp())
+
+    print("Sleeping for %s seconds" % (reset_time - datetime.datetime.now().timestamp()))
