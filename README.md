cbc052c1974388bbd040de73210058cfb8269721
Samuel <sshodge2@ncsu.edu>
2022-04-25 23:50:37 -0400

    began adding targets

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # github-mining
+
+### Setup 
+
+1. Create a personal access token on github.com > account settings > developer settings > Personal access tokens
+
+2. Create a .env file at the same level as this README: `touch .env`
+
+3. Paste the token in to '.env' file as `GITHUB_TOKEN=<your token>`
+
+4. install python dependencies: `pip3 install -r requirements.txt`
+
+### Usage
--- find_repos.py
+++ find_repos.py
@@ -4,35 +4,98 @@ from github import Github       #pip3 install pyGithub
 from decouple import config     #pip3 install python-decouple
 from time import sleep
 import datetime
+import re
 
 from os import path
 
-search_term_list = ['behaviortree_ros']
+def main():
 
-if not path.exists('.env'):
-    raise Exception('Please create .env file with the following content:\n\nGITHUB_TOKEN=')
+    keywords = ['ros','ros2']
+    qualifiers = ['stars:>2', 'language:python']
+    # languages = ['Python','C++']
+    # sort = ''       #('stars', 'forks', 'updated')
+    # order = ''      #('asc', 'desc')
 
-github_oaut_token = config('GITHUB_TOKEN')
+    # checked_repos = set()
 
-g = Github(login_or_token=github_oaut_token)
+    searchable_files = ['.*.py', 'CMakeLists.txt', 'package.xml'] 
+
+    targets = ['test']
+
+    if not path.exists('.env'):
+        raise Exception('Please create .env file with the following content:\n\nGITHUB_TOKEN=')
+
+    github_oaut_token = config('GITHUB_TOKEN')
+
+    g = Github(login_or_token=github_oaut_token)
 
 
-remaining, request_limit = g.rate_limiting
+    remaining, request_limit = g.rate_limiting
 
-print("Remaining: %s, Limit: %s" % (remaining, request_limit))
+    print("Remaining: %s, Limit: %s" % (remaining, request_limit))
 
-while(True):
+    while(True):
 
-    while(remaining > 1):
-        sleep(10)
-        remaining, request_limit = g.rate_limiting
-        # print("Remaining: %s, Limit: %s" % (remaining, request_limit))
+        try:
 
-    
+            while(remaining > 1):
 
-    reset_time = g.rate_limiting_resettime
+                query = ' '.join(keywords) + ' ' + ' '.join(qualifiers)
 
-    # sleep until the reset time
-    sleep(reset_time - datetime.datetime.now().timestamp())
+                print(query)
 
-    print("Sleeping for %s seconds" % (reset_time - datetime.datetime.now().timestamp()))
+                repos = g.search_repositories(query=query)
+
+                # print(repos)
+
+                for repo in repos:
+
+                    print("")
+
+                    contents = repo.get_contents("")
+
+                    # repo.get
+
+                    while contents:
+                        file_content = contents.pop(0)
+                        if file_content.type == "dir":
+                            contents.extend(repo.get_contents(file_content.path))
+                        else:
+
+                            file_name = file_content.path.split('/')[-1]
+                            print(file_name)
+
+                            # check if file is in searchable_files (with regex)
+                            if any(re.search(pattern, file_name) for pattern in searchable_files):
+                                print("Found file: %s" % file_name)
+
+                                code = file_content.decoded_content.decode('utf-8', 'ignore')
+                                # print(code)
+
+                                #TODO check if code contains any of the targets
+                                #if yes, save the repo
+                    
+                remaining, request_limit = g.rate_limiting
+                print("Remaining: %s, Limit: %s" % (remaining, request_limit))
+                sleep(100)
+                
+        except KeyboardInterrupt:
+            print("\nExiting early...")
+            print(remaining , " github requests remaining")
+            return False
+
+        reset_time = g.rate_limiting_resettime
+
+        try:
+
+            # sleep until the reset time
+            print("Sleeping for %s seconds" % (reset_time - datetime.datetime.now().timestamp()))
+            sleep(reset_time - datetime.datetime.now().timestamp())
+            
+        except KeyboardInterrupt:
+            print("\nExiting timeout")
+            return False
+
+
+if __name__ == '__main__':
+    main()
