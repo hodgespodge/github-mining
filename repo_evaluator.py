237e2b9a61770cd99666fa7a0a783f579dcc1528
hodgespodge <samuelhodges3000@gmail.com>
2022-04-28 17:43:15 -0400

    got repo name evaluator working

--- repo_evaluator.py
+++ repo_evaluator.py
@@ -1,3 +1,4 @@
+from re import A
 from github import Repository, ContentFile
 
 import json
@@ -30,7 +31,6 @@ class repo_evaluator:
         equation = equation.replace('and', '&').replace('or', '|').replace('not', '~').replace('!', '~')
         equation = sy.parsing.sympy_parser.parse_expr(equation, evaluate=False)
         equation = sy.to_dnf(equation, simplify=True)
-        print(type(type(equation)))
 
         return equation
 
@@ -38,10 +38,6 @@ class repo_evaluator:
 
         code_targets['equation'] = self.clean_equation(code_targets['equation'])
         code_targets['args'] = self.get_expr_arg_dict(code_targets['equation'])
-
-        # print("Code equation: " + str(code_targets['equation']))
-
-        print("Code targets:" + str(code_targets)+"\n")
 
         self.check_args_exist_in_dict(code_targets['args'], code_targets)
 
@@ -56,10 +52,6 @@ class repo_evaluator:
         file_targets['equation'] = self.clean_equation(file_targets['equation'])
         file_targets['args'] = self.get_expr_arg_dict(file_targets['equation'])
 
-        # print("File equation: " + str(file_targets['equation']))
-
-        print("File targets:" + str(file_targets)+"\n")
-
         self.check_args_exist_in_dict(file_targets['args'], file_targets)
 
         for arg in file_targets['args']:
@@ -69,14 +61,10 @@ class repo_evaluator:
                 
                 if target_type == None:
                     raise Exception("Target must have a 'target_type' key")
-                if target_type == "repo":
-                    raise Exception("Cannot have a repo target inside a file")
-                elif target_type == "dir":
-                    raise Exception("Cannot have a dir target inside a file")
-                elif target_type == "file":
-                    raise Exception("Cannot have a file target inside a file")
+                elif target_type == "repo" or target_type == "dir" or target_type == "file":
+                    raise Exception("Cannot have a "+ target_type +" target inside a file")
                 elif target_type == "code":
-                    target = self.__init_code_targets(target)
+                    file_targets[str(arg)] = self.__init_code_targets(target)
                 else:
                     raise Exception("Target type not recognized: " + target["target_type"])
 
@@ -87,10 +75,6 @@ class repo_evaluator:
         dir_targets['equation'] = self.clean_equation(dir_targets['equation'])
         dir_targets['args'] = self.get_expr_arg_dict(dir_targets['equation'])
 
-        # print("Dir equation: " + str(dir_targets['equation']))
-
-        print("Dir targets:" + str(dir_targets)+"\n")
-
         self.check_args_exist_in_dict(dir_targets['args'], dir_targets)
 
         for arg in dir_targets['args']:
@@ -100,12 +84,10 @@ class repo_evaluator:
 
                 if target_type == None:
                     raise Exception("Target must have a 'target_type' key")
-                if target_type == "repo":
-                    raise Exception("Cannot have a repo target inside a dir")
-                elif target_type == "dir":
-                    raise Exception("Cannot have a dir target inside a dir")
+                elif target_type == "repo" or target_type == "dir":
+                    raise Exception("Cannot have a " + target_type + "target inside a dir")
                 elif target_type == "file":
-                    target = self.__init_file_targets(target)
+                    dir_targets[str(arg)] = self.__init_file_targets(target)
                 elif target_type == "code":
 
                     wrapper_file_target = {
@@ -114,11 +96,7 @@ class repo_evaluator:
                         "equation": "x1",
                     }
 
-                    print("initing file target with dummy wrapper")
-
-                    # TODO: need to add (always true file to wrap code requirement)
-                    # target = self.__init_code_targets(target)
-                    target = self.__init_file_targets(wrapper_file_target)
+                    dir_targets[str(arg)] = self.__init_file_targets(wrapper_file_target)
 
                 else:
                     raise Exception("Target type not recognized: " + target["target_type"])
@@ -129,10 +107,6 @@ class repo_evaluator:
 
         repo_targets['equation'] = self.clean_equation(repo_targets['equation'])
         repo_targets['args'] = self.get_expr_arg_dict(repo_targets['equation'])
-
-        # print("Repo equation: " + str(repo_targets['equation']))
-
-        print("Repo targets:" + str(repo_targets)+"\n")
 
         self.check_args_exist_in_dict(repo_targets['args'], repo_targets)
 
@@ -147,7 +121,7 @@ class repo_evaluator:
                 if target_type == "repo":
                     raise Exception("Cannot have a repo target inside a repo")
                 elif target_type == "dir":
-                    target = self.__init_dir_targets(target)
+                    repo_targets[str(arg)] = self.__init_dir_targets(target)
                 elif target_type == "file" or target_type == "code":
 
                     wrapper_dir_target = {
@@ -156,16 +130,12 @@ class repo_evaluator:
                         "equation": "x1",
                     }
 
-                    print("initing dir target with dummy wrapper")
-                    # TODO: need to add (always true dir to wrap file requirement)
-                    target = self.__init_dir_targets(wrapper_dir_target)
+                    repo_targets[str(arg)] = self.__init_dir_targets(wrapper_dir_target)
 
-                # elif target_type == "code":
-                #     # TODO: need to add (always true file to wrap code requirement)
-                #     target = self.__init_code_targets(target)
                 else:
                     raise Exception("Target type not recognized: " + target["target_type"])
         
+
         return repo_targets
 
     def __init_targets(self, targets:dict):
@@ -188,11 +158,6 @@ class repo_evaluator:
                 }
                 targets = self.__init_repo_targets(wrapper_repo_target)
 
-                # targets = self.__init_dir_targets(targets)
-            # elif target_type == "file":
-            #     targets = self.__init_file_targets(targets)
-            # elif target_type == "code":
-            #     targets = self.__init_code_targets(targets)
             else:
                 raise Exception("Target type not recognized: " + targets["target_type"])
         else:
@@ -211,19 +176,31 @@ class repo_evaluator:
 
         self.targets = self.__init_targets(self.targets)
 
-        print()
-        print("Targets: " + str(self.targets))
-        print()
     def evaluate_equation(self, equation: sy.Expr, args: dict):
-        print("evaluating equation: " + str(equation))
-        print("args: " + str(args))
+        
         equation_result = equation.subs(args)
-        print("equation result: " + str(equation_result))
-
-        if type(equation_result) == bool:
-            return equation_result
+        if type(equation_result) == sy.logic.boolalg.BooleanTrue:
+            return True
+        elif type(equation_result) == sy.logic.boolalg.BooleanFalse:
+            return False
         else:
-            print("")
+            return None
+
+
+    # TODO Need to rethink how I handle nested directories
+    # Also may need to add a variable for defining if recursive search through directories is allowed
+    def eval_dir_targets(self, repo: Repository.Repository, contents : ContentFile.ContentFile, targets : dict):
+
+        dir_args = targets['args']
+
+        for arg in dir_args:
+            
+            arg_regex = targets[str(arg)]
+
+            if type(arg_regex) == str:
+                dir_args[arg] = in_regex(arg_regex, contents.get_name())
+
+
 
     def eval_repo_targets(self, repo : Repository.Repository, contents : ContentFile.ContentFile, targets : dict):
 
@@ -236,12 +213,24 @@ class repo_evaluator:
             if type(arg_regex) == str: # else it's a dict
                 repo_args[arg] = in_regex( arg_regex, repo.name)
 
-        equation_result = self.evaluate_equation(targets['equation'], repo_args)
+            equation_result = self.evaluate_equation(targets['equation'], repo_args)
 
-        # print(equation_result)
+            if equation_result is not None:
+                return equation_result
 
-        # loop and eval dir targets. 
-        # If the equation_result returns a bool value at any point, return it
+        for arg in repo_args.keys():
+            
+            if repo_args[arg] != None:
+                continue
+            elif type(targets[str(arg)]) == dict:
+                repo_args[arg] = self.eval_dir_targets(repo, contents, targets[str(arg)])
+            else:
+                raise Exception("An error occured while trying to evaluate repo arg: " + str(arg))
+
+            equation_result = self.evaluate_equation(targets['equation'], repo_args)
+
+            if equation_result is not None:
+                return equation_result
 
         return equation_result
 
@@ -257,64 +246,17 @@ class repo_evaluator:
 
         if targets['target_type'] == "repo":
             return self.eval_repo_targets(repo, contents, targets)
-        # elif targets['target_type'] == "dir":
-        #     return self.eval_dir_targets(repo, contents, targets)
-        # elif targets['target_type'] == "file":
-        #     return self.eval_file_targets(repo, contents, targets)
-        # elif targets['target_type'] == "code":
-        #     return self.eval_code_targets(repo, contents, targets)
+
         else:
-            raise Exception("Target type not recognized: " + targets["target_type"])
+            raise Exception("Some error occured while initializing the targets")
 
-        # while contents:
-        #     file_content = contents.pop(0)
-        #     if file_content.type == "dir":
-
-        #         folder_name_found, folder_name_pattern = in_regex_list(file_content.name, dir_targets)
-
-        #         if folder_name_found:
-        #             return True, file_content.name, folder_name_pattern
-
-
-        #         contents.extend(repo.get_contents(file_content.path))
-        #     else:
-
-        #         file_name = file_content.path.split('/')[-1]
-        #         # print(file_name)
-
-        #         if file_content.size > max_file_size:
-        #             continue
-
-        #         # check if file is in files_targets (with regex)
-        #         file_type_found , file_type_target = in_regex_list(file_name, files_targets.keys()) 
-
-        #         if file_type_found:
-        #             try:
-        #                 code = str(file_content.decoded_content.decode('utf-8', 'ignore'))
-                        
-        #                 #check if code string contains any of the targets in target using regex
-        #                 for target in files_targets[file_type_target]:
-        #                     # print("Checking if target: ", target, " is in file: ", file_name)
-
-        #                     if re.search(target, code):
-        #                         print("Found Target: \"", target,"\"")
-        #                         # print(code)
-        #                         return True, file_name, target
-        #             except AssertionError:
-        #                 print("Error decoding file: ", file_content.path)
-                    
-        # return False, "", ""    #code_target_found, target_file_name, target 
-
-
-
-    
 def main():
 
     import os
     from github import Github
     from decouple import config   
 
-    with open('searches2.json') as json_file:
+    with open('searches.json') as json_file:
         searches = json.load(json_file)
 
     searcher = repo_evaluator(searches[0])
@@ -330,10 +272,11 @@ def main():
     remaining, request_limit = g.rate_limiting
     print("Remaining: %s, Limit: %s" % (remaining, request_limit))
 
-
     repo = g.get_repo("hodgespodge/github-mining")
 
     print(repo.clone_url)
+
+    print("\nevaling repo: " + str(repo)+"\n")
 
     print(searcher.eval_repository(repo))
 
